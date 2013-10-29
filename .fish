78752fd248793e9bae582231b332a43d2561760d
Joseph Schmitt <me@joe.sh>
2013-10-29 12:55:09 -0700

    Add Fish Shell to initial setup

---
+++ .fish
@@ -0,0 +1,21 @@
+alias cl="clear"
+
+alias joe.sh="cd ~/Dropbox/Documents/Projects/joe.sh"
+alias bashr="source ~/.bash_profile"
+alias gruntsave="npm install grunt --save-dev"
+alias kiddo-app="cd ~/Dropbox/Documents/Projects/Kiddo/Kiddo-Parent-App"
+
+# Git Aliases
+alias gpl="git pull"
+alias gpu="git push"
+alias gs="git status"
+alias gco="git checkout"
+alias gb="git checkout -b"
+alias gbl="git branch"
+alias ga="git add"
+alias gca="git commit -a"
+alias gci="git commit"
+# alias pub="git stash save && git checkout master && git merge dev && git push && git checkout dev && git stash apply"
+
+# Alias for starting sass watch on joe.sh directory
+alias jsass='sass --watch ~/Dropbox/Documents/Projects/joe.sh/site/sass:~/Dropbox/Documents/Projects/joe.sh/assets/styles'
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,25 +1,5 @@
 #!/usr/bin/env bash
-cd "$(dirname "${BASH_SOURCE}")"
-git pull origin master
-function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
-	source ~/.bash_profile
-}
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt
-else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-	echo
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt
-	fi
-fi
-unset doIt
 
-###############################################################################
-# Basic Setup                                                                 #
-###############################################################################
 # Install homebrew
 ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
 
@@ -31,3 +11,26 @@ echo "173.230.151.220 krang" | sudo tee -a /etc/hosts >/dev/null
 
 # Install Cask and native apps
 source .brew-cask
+
+# Configure Bash
+cd "$(dirname "${BASH_SOURCE}")"
+git pull origin master
+function doIt() {
+	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
+		--exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
+	source ~/.bash_profile
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+	doIt
+else
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+	echo
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		doIt
+	fi
+fi
+unset doIt
+
+# Configure Fish
+cp .fish ~/.config/fish/config.fish
