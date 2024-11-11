e77a04900137c1fddef2dc18acb31dc17d4948ec
Joe Schmitt <joe@compass.com>
2024-11-11 12:14:02 -0500

    Fix cdd completions in fish

--- .config/fish/completions/cdd.fish
+++ .config/fish/completions/cdd.fish
@@ -1,1 +1,8 @@
-complete -f -c cdd -a "(command ls -d ~/development/*/ | xargs -n 1 basename)"
+function __fish_complete_cdd
+  # List only directories within ~/development
+  for dir in ~/development/*/
+    echo (basename "$dir")
+  end
+end
+
+complete -c cdd -f -a '(__fish_complete_cdd)'
