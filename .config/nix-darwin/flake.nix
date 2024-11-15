55e0bf5b72a602c60d6db979febbfd7457e25368
Joe Schmitt <joe@compass.com>
2024-11-15 16:45:19 -0500

    Fix bugs in iterm and app install

--- .config/nix-darwin/flake.nix
+++ .config/nix-darwin/flake.nix
@@ -17,7 +17,7 @@
           pkgs.fish
           pkgs.fzf
           pkgs.gh
-          pkgs.iTerm2
+          pkgs.iterm2
           pkgs.stow
           pkgs.neovim
         ];
@@ -35,7 +35,7 @@
         rm -rf /Applications/Nix\ Apps
         mkdir -p /Applications/Nix\ Apps
         find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-        while read src; do
+        while read -r src; do
           app_name=$(basename "$src")
           echo "copying $src" >&2
           ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
