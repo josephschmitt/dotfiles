fc24481619cd63627073d32d4ef0708743ccb25b
Joe Schmitt <joe@compass.com>
2025-01-03 21:52:28 -0500

    Configure neovim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+return {
+  -- Swap redo and undo line
+  { "n", "U", "<C-r>", { desc = "Redo" } },
+  { "n", "<C-r>", "U", { desc = "Undo Line" } },
+
+  -- Buffer fuzzy finder
+  { "n", "<leader>bf", ":Telescope buffers<CR>", { desc = "Fuzzy Search Buffers" } },
+}
---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,8 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  opts = {
+    defaults = {
+      file_ignore_patterns = { "node_modules", "dist", ".git" }, -- Add directories to ignore here
+    },
+  },
+}
