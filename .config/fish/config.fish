662eaab898555a3c6cf4cda6892081b9a9a74e6b
Joe Schmitt <joe@compass.com>
2025-01-30 13:36:38 -0500

    Rename nix_udpate to darwin_update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -50,7 +50,7 @@ set -gx PATH "$HOME/development/zide/bin" $PATH
 alias gr="cd (git rev-parse --show-toplevel)"
 alias zellij_clear="zellij list-sessions --no-formatting | awk '/EXITED/ {print \$1}' | xargs -n 1 zellij delete-session"
 alias darwin_rebuild="darwin-rebuild switch --flake ~/dotfiles/.config/nix-darwin"
-alias nix_update="nix flake update --flake ~/dotfiles/.config/nix-darwin"
+alias darwin_update="nix flake update --flake ~/dotfiles/.config/nix-darwin"
 
 # Git-spice aliases
 alias gsb="gs branch"
