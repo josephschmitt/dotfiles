1fa66562be0c9928ca843eb35c962c58b08ec43b
Joe Schmitt <joe@compass.com>
2025-02-08 13:21:42 -0500

    Fix path to fzfignore

--- .config/nvim/lua/plugins/fzf-lua.lua
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -3,7 +3,7 @@ return {
     "ibhagwan/fzf-lua",
     opts = {
       files = {
-        fd_opts = [[--hidden --exclude .git --exclude node_modules --ignore-file ~/.ignore]],
+        fd_opts = [[--hidden --exclude .git --exclude node_modules --ignore-file ~/.fzfignore]],
       },
     },
   },
