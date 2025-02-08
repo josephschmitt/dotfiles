803043fa09d18e69ce7ef8cd34207c21f8f3911d
Joe Schmitt <joe@compass.com>
2025-02-08 13:35:24 -0500

    Comment out Alt-based window movements
    Trying to get used to default Ctrl-based, and use Alt + hjkl for moving
    lines.

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -11,11 +11,11 @@ map({ "n", "v" }, "gh", "^", { desc = "Go to beginning of line" })
 map({ "n", "v" }, "gl", "$", { desc = "Go to end of line" })
 
 -- Window commands
-map({ "n" }, "<A-h>", "<C-w>h", { desc = "Go to the left window" })
-map({ "n" }, "<A-l>", "<C-w>l", { desc = "Go to the right window" })
-map({ "n" }, "<A-j>", "<C-w>j", { desc = "Go to the down window" })
-map({ "n" }, "<A-k>", "<C-w>k", { desc = "Go to the up window" })
-map({ "n" }, "<A-w>", "<C-w>w", { desc = "Switch windows" })
+-- map({ "n" }, "<A-h>", "<C-w>h", { desc = "Go to the left window" })
+-- map({ "n" }, "<A-l>", "<C-w>l", { desc = "Go to the right window" })
+-- map({ "n" }, "<A-j>", "<C-w>j", { desc = "Go to the down window" })
+-- map({ "n" }, "<A-k>", "<C-w>k", { desc = "Go to the up window" })
+-- map({ "n" }, "<A-w>", "<C-w>w", { desc = "Switch windows" })
 map({ "n" }, "<A-s>", "<C-w>s", { desc = "Split window" })
 map({ "n" }, "<A-v>", "<C-w>v", { desc = "Split window vertically" })
 
