4df04d7ddb880005421da560d37b590cc8a952dd
Joe Schmitt <joe@compass.com>
2025-02-07 20:44:10 -0500

    More helix-style keymapping for nvim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -3,7 +3,18 @@
 -- Add any additional keymaps here
 local map = vim.keymap.set
 
-map({ "n", "i", "v" }, "U", "<C-r>", { desc = "Redo" })
+-- Undo/redo
+map({ "n", "v" }, "U", "<C-r>", { desc = "Redo" })
 
-map({ "n", "i", "v" }, "gh", "^", { desc = "Go to beginning of line" })
-map({ "n", "i", "v" }, "gl", "$", { desc = "Go to end of line" })
+-- Goto commands
+map({ "n", "v" }, "gh", "^", { desc = "Go to beginning of line" })
+map({ "n", "v" }, "gl", "$", { desc = "Go to end of line" })
+
+-- Window commands
+map({ "n" }, "<A-h>", "<C-w>h", { desc = "Go to the left window" })
+map({ "n" }, "<A-l>", "<C-w>l", { desc = "Go to the right window" })
+map({ "n" }, "<A-j>", "<C-w>j", { desc = "Go to the down window" })
+map({ "n" }, "<A-k>", "<C-w>k", { desc = "Go to the up window" })
+map({ "n" }, "<A-w>", "<C-w>w", { desc = "Switch windows" })
+map({ "n" }, "<A-s>", "<C-w>s", { desc = "Split window" })
+map({ "n" }, "<A-v>", "<C-w>v", { desc = "Split window vertically" })
