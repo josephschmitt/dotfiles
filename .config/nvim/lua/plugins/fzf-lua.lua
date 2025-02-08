c884be55718bb524a75e99b6bae97aebb4124c71
Joe Schmitt <joe@compass.com>
2025-02-08 17:03:28 -0500

    Use snacks.nvim for explorer and picker

--- .config/nvim/lua/plugins/fzf-lua.lua
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -1,6 +1,7 @@
 return {
   {
     "ibhagwan/fzf-lua",
+    enabled = false,
     opts = {
       files = {
         fd_opts = [[--hidden --exclude .git --exclude node_modules --ignore-file ~/.fzfignore]],
---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    explorer = {
+      enabled = true,
+      hidden = true, -- Show hidden files by default
+    },
+    picker = {
+      enabled = true,
+      hidden = true, -- Show hidden files by default
+    },
+  },
+}
