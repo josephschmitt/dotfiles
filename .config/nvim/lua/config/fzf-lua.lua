0b13302d086ee7ccc3840b9188ae5dad0c43dd8a
Joe Schmitt <joe@compass.com>
2025-02-07 20:44:26 -0500

    Add fzfignore file to ignore certain dirs

---
+++ .config/nvim/lua/config/fzf-lua.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "ibhagwan/fzf-lua",
+    opts = {
+      files = {
+        fd_opts = [[--hidden --exclude .git --exclude node_modules --ignore-file ~/.ignore]],
+      },
+    },
+  },
+}
