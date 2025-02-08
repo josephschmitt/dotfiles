d4f5b66f430d2dcdea1139e0a8fe76f2d5d52544
Joe Schmitt <joe@compass.com>
2025-02-07 20:02:09 -0500

    Neovim keymaps for redo, goto start/end line like helix

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,12 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
 
-return {
-  -- Swap redo and undo line
-  { "n", "U", "<C-r>", { desc = "Redo" } },
-  { "n", "<C-r>", "U", { desc = "Undo Line" } },
+map({ "n", "i", "v" }, "U", "<C-r>", { desc = "Redo" })
 
-  -- Buffer fuzzy finder
-  { "n", "<leader>bf", ":Telescope buffers<CR>", { desc = "Fuzzy Search Buffers" } },
-}
+map({ "n", "i", "v" }, "gh", "^", { desc = "Go to beginning of line" })
+map({ "n", "i", "v" }, "gl", "$", { desc = "Go to end of line" })
