5785ba00dfeedae7edcd8cffd1336ef4031e609c
Joe Schmitt <joe@compass.com>
2025-03-28 23:53:28 -0400

    Update plugins lockfile

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -6,23 +6,32 @@ local map = vim.keymap.set
 -- Undo/redo
 map({ "n", "v" }, "U", "<C-r>", { desc = "Redo" })
 
+-- Comment line
+map({ "n" }, "<C-c>", "gcc", { desc = "Comment line" })
+map({ "v" }, "<C-c>", "gc", { desc = "Comment line" })
+
 -- Goto commands
 map({ "n", "v" }, "gs", "^", { desc = "Go to line first non-blank character" })
 map({ "n", "v" }, "gh", "0", { desc = "Go to beginning of line" })
 map({ "n", "v" }, "gl", "$", { desc = "Go to end of line" })
 map({ "n", "v" }, "ge", "G", { desc = "Go to last line" })
-map({ "n", "v" }, "gj", "G", { desc = "Go to last line" })
-map({ "n", "v" }, "gk", "gg", { desc = "Go to first line" })
 
 -- Window commands
+map({ "n", "v" }, "gw", "<C-w>", { desc = "Window mode" })
 map({ "n" }, "<A-s>", "<C-w>s", { desc = "Split window" })
 map({ "n" }, "<A-v>", "<C-w>v", { desc = "Split window vertically" })
+map({ "n" }, "<A-q>", "<C-w>q", { desc = "Close window" })
+map({ "n" }, "<A-o>", "<C-w>o", { desc = "Close other window" })
+map({ "n" }, "<A-w>", "<C-w>w", { desc = "Switch window" })
 
 -- Helix-like line selection and delete
 map({ "n" }, "<C-a>", "ggVG", { desc = "Select all" })
-map({ "n" }, "%", "ggVG", { desc = "Select all" })
 map({ "n" }, "x", "V", { desc = "Select line" })
 map({ "n" }, "d", "x", { desc = "Delete character" })
 
 map({ "v" }, "x", "j", { desc = "Select next line" })
 map({ "v" }, "X", "k", { desc = "Select prev line" })
+
+-- Exit insert mode
+map({ "i" }, "jk", "<esc>", { desc = "Exit insert mode" })
+map({ "i" }, "jj", "<esc>", { desc = "Exit insert mode" })
