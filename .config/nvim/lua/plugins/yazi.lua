73c7c7517fe4ea2bbf8cc6d4f1c802d9309d328f
Joe Schmitt <joe@compass.com>
2025-03-29 17:36:27 -0400

    Remove unnecessary yazi config

--- .config/nvim/lua/plugins/yazi.lua
+++ .config/nvim/lua/plugins/yazi.lua
@@ -13,20 +13,7 @@ return {
       "<cmd>Yazi cwd<cr>",
       desc = "Open the file manager in nvim's working directory",
     },
-    -- {
-    --   -- NOTE: this requires a version of yazi that includes
-    --   -- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
-    --   "<leader>fY",
-    --   "<cmd>Yazi toggle<cr>",
-    --   desc = "Resume the last yazi session",
-    -- },
   },
   ---@type YaziConfig
-  opts = {
-    -- if you want to open yazi instead of netrw, see below for more info
-    open_for_directories = true,
-    keymaps = {
-      show_help = "<f1>",
-    },
-  },
+  opts = {},
 }
