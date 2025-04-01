b3b065c2da07332a04e4fb2536560b1786ca6ccf
Joe Schmitt <joe@compass.com>
2025-04-01 11:54:43 -0700

    Fix typescript lspconfig with hnvm

---
+++ .config/nvim/lua/plugins/hnvm.lua
@@ -0,0 +1,14 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    setup = {
+      vtsls = function(_, opts)
+        -- Inject the HNVM_NODE variable so vtsls picks the right Node version.
+        opts.cmd_env = {
+          HVNM_QUIET = false, -- Ensure we log hnvm info in cases of failure.
+          HNVM_NODE = "22.14.0", -- Update this version to match your hnvm setup.
+        }
+      end,
+    },
+  },
+}
