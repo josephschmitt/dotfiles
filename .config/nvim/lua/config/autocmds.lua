49d65e31f2f6499daac5e2e3d81884b16cf347b1
Joe Schmitt <joe@compass.com>
2025-04-01 11:54:05 -0700

    Add autocmd to set working dir on startup

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,15 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    if vim.fn.argc() == 1 then
+      local arg0 = vim.fn.argv(0)
+      if vim.fn.isdirectory(arg0) == 1 then
+        vim.cmd("cd " .. arg0)
+        vim.cmd("silent! 0") -- avoid trying to open the dir as a file
+      end
+    end
+  end,
+})
