1c7b1fe155bf95455840eeadc17fae70367ad322
Joe Schmitt <joe@compass.com>
2025-04-01 12:00:09 -0700

    Enable hidden files by default in fzf

--- .config/nvim/lua/plugins/fzf-lua.lua
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -4,6 +4,7 @@ return {
     enabled = true,
     opts = {
       files = {
+        hidden = true,
         fd_opts = [[--hidden --exclude .git --exclude node_modules --ignore-file ~/.fzfignore]],
       },
     },
