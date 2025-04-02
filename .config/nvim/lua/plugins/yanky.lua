02e93392409a9604fac4d935a7c2fda158cbdf95
Joe Schmitt <joe@compass.com>
2025-04-02 16:50:45 -0700

    Remove autolock and fix yanky

--- .config/nvim/lua/plugins/yanky.lua
+++ .config/nvim/lua/plugins/yanky.lua
@@ -1,8 +1,6 @@
 return {
   "gbprod/yanky.nvim",
-  opts = {
-    ring = { storage = "sqlite" },
-  },
+  opts = {},
   keys = {
     { "<leader>p", "<cmd>YankyRingHistory<cr>", mode = { "n", "x" }, desc = "Open Yank History" },
     { "y", "<Plug>(YankyYank)", mode = { "n", "x" }, desc = "Yank text" },
