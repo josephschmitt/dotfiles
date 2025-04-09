860dbb8be4090158e4688b2eee35d048425acfc4
Joe Schmitt <joe@compass.com>
2025-04-08 20:49:42 -0400

    Add copilot plugin and configure super-tab for blink.cmp

---
+++ .config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,13 @@
+local LazyVim = require("lazyvim.util")
+return {
+  "zbirenbaum/copilot.lua",
+  opts = function()
+    LazyVim.cmp.actions.ai_accept = function()
+      if require("copilot.suggestion").is_visible() then
+        LazyVim.create_undo()
+        require("copilot.suggestion").accept()
+        return true
+      end
+    end
+  end,
+}
