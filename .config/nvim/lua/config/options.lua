ccb78234fbc6423cd37e70ffe426d2003a46decd
Joe Schmitt <joe@compass.com>
2025-04-14 14:57:14 -0400

    Move hnvm fix to a global env option instead of just in lspconfig

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,3 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.diagnostic.config({ virtual_lines = true })
+
+-- Force a specific node version for hnvm used in plugins
+vim.env.HNVM_NODE = "22.14.0" -- Update this version to match your hnvm setup.
+vim.env.HVNM_QUIET = false -- Ensure we log hnvm info in cases of failure.
