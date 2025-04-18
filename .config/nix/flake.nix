1af7263c6d8b47f5859c06954ffb7a1b85bbe958
Joe Schmitt <joe@compass.com>
2025-04-18 00:05:26 -0400

    Add nix flake for ubuntu

---
+++ .config/nix/flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Package set for my Ubuntu server";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+  let
+    # Your server’s architecture.  Change if you’re not x86_64.
+    system = "x86_64-linux";
+    pkgs   = import nixpkgs { inherit system; };
+  in {
+    # Everything you want to appear on the machine goes in this list ↓
+    packages.${system}.default = pkgs.buildEnv {
+      name  = "server-env";
+      paths = with pkgs; [
+        fish
+        git      
+        tailscale
+      ];
+    };
+  };
+}
