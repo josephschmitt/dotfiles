1ee31cf3f7e3fef569d76ac2d9872a1570f2e9cb
Joe Schmitt <joe@compass.com>
2025-04-28 15:09:41 -0400

    Fix concealing of markdown codeblock headings

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -7,6 +7,7 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
+-- Automatically set the current working directory when focusing a directory
 vim.api.nvim_create_autocmd("VimEnter", {
   callback = function()
     if vim.fn.argc() == 1 then
@@ -16,5 +17,13 @@ vim.api.nvim_create_autocmd("VimEnter", {
         vim.cmd("silent! 0") -- avoid trying to open the dir as a file
       end
     end
+  end,
+})
+
+-- Disable conceal level for markdown files
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = { "*.md", "*.markdown" }, -- or "*" if you want it globally
+  callback = function()
+    vim.opt.conceallevel = 0
   end,
 })
