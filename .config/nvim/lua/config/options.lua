09f5a1d2273ad3a837a7621cab67cd685cd58989
Joe Schmitt <joe@compass.com>
2025-05-01 21:52:38 -0400

    Use _ for visual mode

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -6,3 +6,11 @@ vim.diagnostic.config({ virtual_lines = true })
 -- Force a specific node version for hnvm used in plugins
 vim.env.HNVM_NODE = "22.14.0" -- Update this version to match your hnvm setup.
 vim.env.HVNM_QUIET = false -- Ensure we log hnvm info in cases of failure.
+
+vim.opt.guicursor = {
+  "n-v-c:block", -- Normal, visual, and command mode: block cursor
+  "v:hor10", -- Override visual mode: horizontal line (underscore)
+  "i-ci:ver25", -- Insert and command-insert: vertical bar
+  "r-cr:hor20", -- Replace and command-replace: horizontal bar
+  "o:hor50", -- Operator-pending: thick horizontal
+}
