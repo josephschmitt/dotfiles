3e96ecca2ccb9d07389193fbe2d9f56b0939dd74
Joe Schmitt <joe@compass.com>
2025-05-04 01:11:27 +0000

    Add osc52.lua to sync yanks to remote clipboard

---
+++ .config/nvim/lua/plugins/osc52.lua
@@ -0,0 +1,22 @@
+-- ~/.config/nvim/lua/plugins/osc52.lua
+return {
+  "ojroques/nvim-osc52",
+  config = function()
+    require("osc52").setup {
+      max_length = 0, -- no limit
+      trim = false,
+      silent = false,
+    }
+
+    -- Automatically copy yanks
+    local function copy()
+      if vim.v.event.operator == "y" and vim.v.event.regname == "" then
+        require("osc52").copy_register("")
+      end
+    end
+
+    vim.api.nvim_create_autocmd("TextYankPost", {
+      callback = copy,
+    })
+  end,
+}
