7c76faeb6261fc5e0855cd892442e0899d293ccf
Joe Schmitt <joe@compass.com>
2025-05-21 14:41:35 -0400

    Fix copilot plugin import

--- .config/nvim/lua/plugins/copilot-chat.lua
+++ .config/nvim/lua/plugins/copilot-chat.lua
@@ -19,7 +19,7 @@ local prompts = {
 }
 
 return {
-  { import = "plugins.extras.copilot-vim" }, -- Or use { import = "lazyvim.plugins.extras.coding.copilot" },
+  { import = "lazyvim.plugins.extras.ai.copilot" },
   {
     "MeanderingProgrammer/render-markdown.nvim",
     optional = true,
