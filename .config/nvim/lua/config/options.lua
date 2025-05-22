d4f01cb0ce62777cc2a97f1bf0f74a4336597082
Joe Schmitt <joe@compass.com>
2025-05-22 10:55:34 -0400

    Disable root level diagnostic virtual lines
    Using tiny diagnostics plugin

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,7 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.diagnostic.config({ virtual_lines = true })
 
 -- Force a specific node version for global hnvm
 vim.env.HNVM_NODE = "22.14.0"
