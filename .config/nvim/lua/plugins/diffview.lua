7226592f08d97ab806f319e02708d2637014205b
Joe Schmitt <joe@compass.com>
2025-05-22 10:54:25 -0400

    Add diffview nvim plugin

---
+++ .config/nvim/lua/plugins/diffview.lua
@@ -0,0 +1,15 @@
+return {
+  "sindrets/diffview.nvim",
+  keys = {
+    {
+      "<leader>gD",
+      "<cmd>DiffviewOpen<cr>",
+      desc = "Diff View (open)",
+    },
+    {
+      "<leader>gH",
+      "<cmd>DiffviewFileHistory %<cr>",
+      desc = "Buffer History (open)",
+    },
+  },
+}
