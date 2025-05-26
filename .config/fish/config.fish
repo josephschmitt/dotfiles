10557593fc1f52cc4cfc0cbe4185e149ebbfbffa
Joe Schmitt <joe@compass.com>
2025-05-26 09:59:41 -0400

    Fix fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -62,57 +62,58 @@ if status is-interactive
     if type -q zoxide
         zoxide init fish | source
     end
+end
 
-    # Add some more bin paths to PATH for custom bin scripts
-    set -gx PATH "$HOME/bin" $PATH
-    set -gx PATH "$HOME/go/bin" $PATH
-    set -gx PATH "$HOME/.local/bin" $PATH
-    set -gx PATH "$HOME/development/zide/bin" $PATH
-    set -gx PATH "$HOME/development/zj/bin" $PATH
+# Add some more bin paths to PATH for custom bin scripts
+set -gx PATH "$HOME/bin" $PATH
+set -gx PATH "$HOME/go/bin" $PATH
+set -gx PATH "$HOME/.local/bin" $PATH
+set -gx PATH "$HOME/development/zide/bin" $PATH
+set -gx PATH "$HOME/development/zj/bin" $PATH
 
-    # Configure asdf 
-    if test -z $ASDF_DATA_DIR
-        set _asdf_shims "$HOME/.asdf/shims"
-    else
-        set _asdf_shims "$ASDF_DATA_DIR/shims"
-    end
+# Configure asdf 
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
 
-    alias groot="echo 'I am Groot!' && cd (git rev-parse --show-toplevel)"
-    alias darwin_rebuild="darwin-rebuild switch --flake ~/dotfiles/.config/nix-darwin"
-    alias darwin_update="nix flake update --flake ~/dotfiles/.config/nix-darwin"
-    alias nix_rebuild="nix profile upgrade --all"
-    alias nix_update="nix flake update --flake ~/dotfiles/.config/nix"
-    alias lg="lazygit"
+alias groot="echo 'I am Groot!' && cd (git rev-parse --show-toplevel)"
+alias darwin_rebuild="darwin-rebuild switch --flake ~/dotfiles/.config/nix-darwin"
+alias darwin_update="nix flake update --flake ~/dotfiles/.config/nix-darwin"
+alias nix_rebuild="nix profile upgrade --all"
+alias nix_update="nix flake update --flake ~/dotfiles/.config/nix"
+alias lg="lazygit"
 
-    # Git-spice aliases
-    alias gsb="gs branch"
-    alias gsbc="gs branch checkout"
-    alias gsbcr="gs branch create"
-    alias gsbs="gs branch submit"
-    alias gsbt="gs branch track"
+# Git-spice aliases
+alias gsb="gs branch"
+alias gsbc="gs branch checkout"
+alias gsbcr="gs branch create"
+alias gsbs="gs branch submit"
+alias gsbt="gs branch track"
 
-    alias gsc="gs commit"
-    alias gsca="gs commit amend"
-    alias gscc="gs commit commit"
+alias gsc="gs commit"
+alias gsca="gs commit amend"
+alias gscc="gs commit commit"
 
-    alias gsl="gs log long -a"
-    alias gsll="gs log long"
-    alias gsls="gs log short"
+alias gsl="gs log long -a"
+alias gsll="gs log long"
+alias gsls="gs log short"
 
-    alias gsr="gs repo"
-    alias gsrs="gs repo sync"
+alias gsr="gs repo"
+alias gsrs="gs repo sync"
 
-    alias gss="gs stack"
-    alias gssr="gs stack restack"
-    alias gsss="gs stack submit"
+alias gss="gs stack"
+alias gssr="gs stack restack"
+alias gsss="gs stack submit"
 
-    # pnpm
-    set -gx PNPM_HOME /Users/josephschmitt/Library/pnpm
-    if not string match -q -- $PNPM_HOME $PATH
-        set -gx PATH "$PNPM_HOME" $PATH
-    end
-    # pnpm end
+# pnpm
+set -gx PNPM_HOME /Users/josephschmitt/Library/pnpm
+if not string match -q -- $PNPM_HOME $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
 
-    if type -q replay
-        replay "source $HOME/.compassrc"
-    end
+if type -q replay
+    replay "source $HOME/.compassrc"
+end
