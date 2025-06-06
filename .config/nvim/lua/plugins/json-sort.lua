fd799fc6a61f113446be2651246c458be79d4dc1
Joe Schmitt <joe@compass.com>
2025-06-06 10:44:11 -0400

    Add json sort nvim plugin

---
+++ .config/nvim/lua/plugins/json-sort.lua
@@ -0,0 +1,13 @@
+return {
+  "2nthony/sortjson.nvim",
+  cmd = {
+    "SortJSONByAlphaNum",
+    "SortJSONByAlphaNumReverse",
+    "SortJSONByKeyLength",
+    "SortJSONByKeyLengthReverse",
+  },
+  opts = {
+    jq = "jq",
+    log_level = "WARN",
+  },
+}
