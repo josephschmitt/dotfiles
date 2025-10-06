4213ee65e67e5a411be08a066214f3cd5d7df7cd
Joe Schmitt <joe@compass.com>
2025-10-06 10:17:33 -0400

    Update nvim plugins to remove augmentcode
    Re-enable avante

--- shared/.config/nvim/lua/plugins/avante.lua
+++ shared/.config/nvim/lua/plugins/avante.lua
@@ -1,7 +1,7 @@
 return {
   "yetone/avante.nvim",
   event = "VeryLazy",
-  enabled = false,
+  enabled = true,
   version = false, -- Never set this value to "*"! Never!
   opts = {
     provider = "copilot",
