c7926f2edb9e65fd771f111b0995f0718958c0c7
Joe Schmitt <dev@joe.sh>
2025-11-27 00:30:49 -0500

    Add trap to support closing quick shell with Ctrl-c x2

--- shared/.config/tmux/quick-shell.sh
+++ shared/.config/tmux/quick-shell.sh
@@ -7,6 +7,25 @@ exec fish --init-command '
         echo -n "⚡ "
     end
     
+    # Track Ctrl-c presses for double-tap exit
+    set -g __quick_shell_last_sigint 0
+    
+    # Handle Ctrl-c (SIGINT)
+    function __handle_sigint --on-signal SIGINT
+        set -l now (date +%s)
+        set -l time_diff (math $now - $__quick_shell_last_sigint)
+        
+        if test $time_diff -le 2
+            # Second Ctrl-c within 2 seconds - exit
+            exit 130
+        else
+            # First Ctrl-c - update timestamp and show hint at bottom via tmux
+            set -g __quick_shell_last_sigint $now
+            tmux display-message -d 2000 "Press Ctrl-c again to close popup"
+            commandline -f repaint
+        end
+    end
+    
     # Hook that runs after each command
     function on_command_execute --on-event fish_postexec
         # Exit with the command status (popup closes on success)
