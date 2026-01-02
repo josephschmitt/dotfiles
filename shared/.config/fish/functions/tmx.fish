d20b312ddabdec9a7c0cf6bce462e2dd635e5102
Joe Schmitt <joe@compass.com>
2026-01-02 13:56:39 -0500

    tmx re-attaches to last session

--- shared/.config/fish/functions/tmx.fish
+++ shared/.config/fish/functions/tmx.fish
@@ -1,8 +1,21 @@
 function tmx
-    set -l dir (if test (count $argv) -gt 0; echo $argv[1]; else; pwd; end)
+    # If no arguments provided, attach to last session
+    if test (count $argv) -eq 0
+        if test -n "$TMUX"
+            # Already in tmux, can't attach - switch to last session instead
+            tmux switch-client -l
+        else
+            # Not in tmux, attach to last used session
+            tmux attach
+        end
+        return
+    end
+
+    # Directory-based session management (when argument provided)
+    set -l dir $argv[1]
     cd $dir; or return 1
     set -l session_name (basename $dir)
-    
+
     if test -n "$TMUX"
         # Already in tmux, switch to session instead of nesting
         tmux new-session -A -d -s $session_name
@@ -10,6 +23,5 @@ function tmx
     else
         # Not in tmux, start new session normally
         tmux new-session -A -s $session_name
-        exit
     end
 end
