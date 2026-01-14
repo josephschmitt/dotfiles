56a56f665a51486c59b0e37932bc82ff42491202
Joe Schmitt <joe@compass.com>
2026-01-14 01:11:01 -0500

    Customize neo-tree width

--- shared/.config/astronvim/lua/plugins/filetree.lua
+++ shared/.config/astronvim/lua/plugins/filetree.lua
@@ -6,21 +6,11 @@ return {
   opts = {
     window = {
       position = "left",
-      width = 30,
+      width = 40,
     },
     filesystem = {
       follow_current_file = true,
       use_libuv_file_watcher = true,
     },
   },
-  init = function()
-    -- Open neo-tree on VimEnter if no file is being edited
-    vim.api.nvim_create_autocmd("VimEnter", {
-      callback = function()
-        if vim.fn.argc() == 0 then
-          require("neo-tree.command").execute({ action = "show" })
-        end
-      end,
-    })
-  end,
 }
