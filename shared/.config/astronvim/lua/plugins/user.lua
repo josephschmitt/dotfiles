42c5918a4af77b967d0a32be57766e3137a8a709
Joe Schmitt <dev@joe.sh>
2026-02-01 20:50:59 -0500

    Disable gitsigns and mini.diff conflicting mappings with gh/gl

--- shared/.config/astronvim/lua/plugins/user.lua
+++ shared/.config/astronvim/lua/plugins/user.lua
@@ -171,6 +171,68 @@ return {
     end,
   },
 
+  -- Override gitsigns to change gh prefix to <leader>gh (freeing gh for goto)
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      on_attach = function(bufnr)
+        local gs = require "gitsigns"
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        map("n", "]g", function()
+          if vim.wo.diff then return "]c" end
+          vim.schedule(function() gs.next_hunk() end)
+          return "<Ignore>"
+        end, { expr = true, desc = "Next Git hunk" })
+        map("n", "[g", function()
+          if vim.wo.diff then return "[c" end
+          vim.schedule(function() gs.prev_hunk() end)
+          return "<Ignore>"
+        end, { expr = true, desc = "Previous Git hunk" })
+
+        -- Actions under <leader>g instead of gh
+        map("n", "<leader>gs", gs.stage_hunk, { desc = "Stage hunk" })
+        map("n", "<leader>gr", gs.reset_hunk, { desc = "Reset hunk" })
+        map("v", "<leader>gs", function() gs.stage_hunk { vim.fn.line ".", vim.fn.line "v" } end, { desc = "Stage hunk" })
+        map("v", "<leader>gr", function() gs.reset_hunk { vim.fn.line ".", vim.fn.line "v" } end, { desc = "Reset hunk" })
+        map("n", "<leader>gS", gs.stage_buffer, { desc = "Stage buffer" })
+        map("n", "<leader>gu", gs.undo_stage_hunk, { desc = "Undo stage hunk" })
+        map("n", "<leader>gR", gs.reset_buffer, { desc = "Reset buffer" })
+        map("n", "<leader>gp", gs.preview_hunk, { desc = "Preview hunk" })
+        map("n", "<leader>gb", function() gs.blame_line { full = true } end, { desc = "Blame line" })
+        map("n", "<leader>gd", gs.diffthis, { desc = "Diff this" })
+        map("n", "<leader>gD", function() gs.diffthis "~" end, { desc = "Diff this ~" })
+
+        -- Text object
+        map({ "o", "x" }, "ig", ":<C-U>Gitsigns select_hunk<CR>", { desc = "Select git hunk" })
+      end,
+    },
+  },
+
+  -- Override mini.diff to disable gh mappings (freeing gh for goto)
+  {
+    "echasnovski/mini.diff",
+    optional = true,
+    opts = {
+      mappings = {
+        -- Disable all default mappings (gh prefix)
+        apply = "",
+        reset = "",
+        textobject = "",
+        goto_first = "",
+        goto_prev = "",
+        goto_next = "",
+        goto_last = "",
+      },
+    },
+  },
+
   -- Mode highlighting with tokyonight colors
   {
     "mvllow/modes.nvim",
